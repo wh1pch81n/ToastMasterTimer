361e571d33671ed8c473d03a8b67e629ca0802f0
wh1pch81n <wh1pch81n@gmail.com>
2014-04-13 17:19:29 -0700

    constraints for the quick panel

--- TMTimer/DHCountDownView.m
+++ TMTimer/DHCountDownView.m
@@ -9,7 +9,12 @@
 #import "DHCountDownView.h"
 
 @interface DHCountDownView ()
+
 @property (strong, nonatomic) UILabel *characterDisplayed;
+@property (nonatomic, assign) int characterIndex;
+@property (nonatomic, copy) void (^complete)();
+@property (strong, nonatomic) NSMutableArray *arrTimers;
+
 @end
 
 @implementation DHCountDownView
@@ -20,7 +25,8 @@
 	if (self) {
        	self.characterDisplayed = [[UILabel alloc] initWithFrame:frame];
 		[self addSubview:self.characterDisplayed];
-		self.backgroundColor = [UIColor grayColor];
+		//self.backgroundColor = [UIColor grayColor];
+        //self.characterDisplayed.backgroundColor = [UIColor grayColor];
 	}
 	return self;
 }
@@ -44,29 +50,60 @@
 		}
 		return;
 	}
+    
+    [self setComplete:complete];
+    
 	NSString *charArr = [self.delegate stringOfCharactersToCountDown];
+    self.characterIndex = 0;
 	for(int delay = 0; delay <= charArr.length; ++delay) {
 
 		double delayInSeconds = [self.delegate characterDelay]* delay;
-		dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-		dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-			if (delay == charArr.length) {
-				if (complete) {
-					complete();
-				}
-			} else {
-				NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:[charArr substringWithRange:NSMakeRange(delay, 1)] attributes:
-																			 @{NSFontAttributeName: [UIFont systemFontOfSize:200]
-																				 }];
-				[self.characterDisplayed setAttributedText:attrStr];
-				[self.characterDisplayed setTextAlignment:NSTextAlignmentCenter];
-//				[UIView animateWithDuration:delayInSeconds/2 animations:^{
-//					self.characterDisplayed.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.5, 0.5);
-//				} completion:Nil];
-				NSLog(@"%@", self.characterDisplayed.text);
-			}
-		});
-	}
+        [self performAfterDelay:delayInSeconds];
+ 	}
+}
+
+/**
+ Creates an nstimer that will fire once after a set amount of time has passed
+ */
+- (void)performAfterDelay:(double)delay
+{
+    [self.arrTimers addObject:[NSTimer scheduledTimerWithTimeInterval:delay
+                                     target:self
+                                   selector:@selector(changeCharacter)
+                                   userInfo:nil
+                                    repeats:NO]];
+}
+
+- (void)completeAnimation {
+    if (self.complete) {
+        self.complete();
+    }
+}
+
+- (void)changeCharacter
+{
+    if (self.characterIndex >= [self.delegate stringOfCharactersToCountDown].length) {
+        [self completeAnimation];
+        return;
+    }
+
+    NSString *charArr = [self.delegate stringOfCharactersToCountDown];
+    NSString *str = [charArr substringWithRange:NSMakeRange(self.characterIndex++, 1)];
+    NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:str
+                                                                  attributes:
+                                   @{
+                                     NSFontAttributeName: [UIFont systemFontOfSize:200],
+                                     
+                                     }];
+    [self.characterDisplayed setAttributedText:attrStr];
+    [self.characterDisplayed setTextAlignment:NSTextAlignmentCenter];
+    
+    self.characterDisplayed.transform = CGAffineTransformScale(CGAffineTransformIdentity, 3, 3);
+    [UIView animateWithDuration:[self.delegate characterDelay]/2 animations:^{
+       self.characterDisplayed.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.5, 1.5);
+    } completion:Nil];
+    
+    NSLog(@"%@", self.characterDisplayed.text);
 }
 
 @end
